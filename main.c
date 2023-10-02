5599c9e78768bc5e689e71d1437ead27a8c8d852
tomnylow <tomnylow2@gmail.com>
2023-10-02 20:26:32 +0300

    Новый обработчик и дребезг

--- main.c
+++ main.c
@@ -1,17 +1,19 @@
 #include <stdint.h>
 #include "MDR32F9Qx_port.h"
 #include "MDR32F9Qx_rst_clk.h"
-#include "MDR32F9Qx_nvic.h"
+
 
 
 #define LED1_PIN    PORT_Pin_2   
 #define LED2_PIN    PORT_Pin_3   
-#define LED3_PIN    PORT_Pin_1   
-#define LED4_PIN    PORT_Pin_5   
+#define LED3_PIN    PORT_Pin_5   
+#define LED4_PIN    PORT_Pin_1   
 
 
 #define LED_ON      1
 #define LED_OFF     0
+
+volatile static uint8_t but_flag = 0;
 
 void delay(uint32_t delay_count) {
     for (volatile uint32_t i = 0; i < delay_count; ++i) {
@@ -41,42 +43,44 @@ void init_leds() {
 }
 void init_button() {
 
-    RST_CLK_PCLKcmd(RST_CLK_PCLK_PORTC, ENABLE);
+    RST_CLK_PCLKcmd(RST_CLK_PCLK_PORTA, ENABLE);
 
 
     PORT_InitTypeDef port_init;
     PORT_StructInit(&port_init);
     port_init.PORT_Pin = PORT_Pin_0;
     port_init.PORT_OE = PORT_OE_IN;
-    PORT_Init(MDR_PORTC, &port_init);
+		port_init.PORT_FUNC = PORT_FUNC_ALTER;
+    PORT_Init(MDR_PORTA, &port_init);
 
-
-    PORT_ITConfig(MDR_PORTC, PORT_Pin_0, ENABLE);
-
-
-    NVIC_EnableIRQ(PORTC_IRQn);
-    NVIC_SetPriority(PORTC_IRQn, 1);
+		NVIC_SetPriority(EXT_INT1_IRQn, 0);
+    NVIC_EnableIRQ(EXT_INT1_IRQn);
+    
 }
 
-void PORTC_IRQHandler(void) {
-
-    if (PORT_GetITStatus(MDR_PORTC, PORT_IT_PIN_0) == 0) {
-
-        //PORT_ClearITPendingBit(MDR_PORTC, PORT_IT_PIN_0);
-     
-        button_counter++;
-
-        PORT_WriteBit(MDR_PORTA, LED1_PIN, (button_counter & 1) ? 1 : 0);
-        PORT_WriteBit(MDR_PORTA, LED2_PIN, (button_counter & 2) ? 1 : 0);
-        PORT_WriteBit(MDR_PORTA, LED3_PIN, (button_counter & 4) ? 1 : 0);
-        PORT_WriteBit(MDR_PORTA, LED4_PIN, (button_counter & 8) ? 1 : 0);
-    }
+void EXT_INT1_IRQHandler(void) {
+		but_flag = 1;
+    
 }
 int main() {
-    // ????????????? ???????????
+	
+		uint8_t button_cnt = 0;
     init_leds();
+		init_button();
+		while(1){
+			if (but_flag) {
 
-    while (1) {
+        //PORT_ClearITPendingBit(MDR_PORTC, PORT_IT_PIN_0);
+				button_cnt++;
+				if (button_cnt > 16) button_cnt = 0;
         
-    }
+        PORT_WriteBit(MDR_PORTC, LED1_PIN, (button_cnt & 1) ? 1 : 0);
+        PORT_WriteBit(MDR_PORTA, LED2_PIN, (button_cnt & 2) ? 1 : 0);
+        PORT_WriteBit(MDR_PORTA, LED3_PIN, (button_cnt & 4) ? 1 : 0);
+        PORT_WriteBit(MDR_PORTA, LED4_PIN, (button_cnt & 8) ? 1 : 0);
+				for(uint32_t i=0; i<3000000; i++);
+				but_flag = 0;
+			}
+		}
+    
 }
